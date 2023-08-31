8de7b8d040b3ccc8a4b6083c129a1ccc056b18d5
willl85 <ngowilfried@gmail.com>
2023-08-31 20:51:24 +0200

    Remove div.content in line 13

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -10,7 +10,7 @@ const Backdrop = (props) => {
 const ModalOverlay = (props) => {
   return (
     <div className={classes.modal}>
-      <div className={classes.content}>{props.children}</div>
+      {props.children}
     </div>
   );
 };
