3a099bcd0357372f1676c6fbda2737277f7d2477
willl85 <ngowilfried@gmail.com>
2023-10-07 17:59:08 +0200

    Add HeaderLoginButton Component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,13 +3,17 @@ import React, { Fragment } from 'react';
 import classes from './Header.module.css';
 import mealsImage from '../../assets/meals.jpg';
 import HeaderCartButton from './HeaderCartButton';
+import HeaderLoginButton from './HeaderLoginButton';
 
 const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>Meals Order</h1>
-        <HeaderCartButton onClick={props.onShownCart} />
+        <div className={classes.button}>
+          <HeaderCartButton onClick={props.onShownCart} />
+          <HeaderLoginButton onClick={props.onShownCart}/>
+        </div>
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImage} alt="A table full of food!" />
