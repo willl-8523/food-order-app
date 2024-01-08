b70d5738a3e439ab443f3f406891d132616a5458
willl85 <ngowilfried@gmail.com>
2024-01-08 19:22:44 +0100

    Add text-btn classes

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -36,7 +36,7 @@ const HeaderCartButton = (props) => {
       <span className={classes.icon}>
         <CartIcon />
       </span>
-      <span> Your Cart</span>
+      <span className={classes["text-btn"]}> Your Cart</span>
       <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
